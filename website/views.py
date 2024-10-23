185406eb086cb36ba4dde6609cae0742bc5f610f
sarinakhandouzi <sarinakhandouzi80@gmail.com>
2024-10-23 22:31:42 +0330

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return HttpResponse ('<h1>home page</h1>')
+    return render(request , 'index.html')
 
 def about_view(request):
-    return HttpResponse('<h1>about page</h1>')
+    return render(request , 'about.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>contact page</h1>')
+    return render(request , 'contact.html')
